ea16424ba4f8df55643a7a7afdb04b43af4b8132
Chinni <910526@cognizant.com>
2021-06-19 15:29:59 +0530

    This is my first project

---
+++ UnitTestProject4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject4")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ef6c082-5de1-4543-b9d9-749ffaa6dfcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject4/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using ClassLibrary4;
+namespace UnitTestProject4
+{
+    [TestFixture]
+    public class SUT
+    {
+
+        [Test]
+        [TestCase("user_11","secret@user11","Welcome user_11!!!")]
+        [TestCase("user_22", "secret@user22", "Welcome user_22!!!")]
+        [TestCase("user_11", "secret@user22", "Invalid user id/password")]
+        public void UnitUnderTest_Scenario_ExpectedOutcome1(string id,string password,string message)
+        {
+            AccountsManager ac = new AccountsManager();
+            string result = ac.ValidateUser(id,password);
+            Assert.That(result, Is.EqualTo(message));
+        }
+    }
+}
